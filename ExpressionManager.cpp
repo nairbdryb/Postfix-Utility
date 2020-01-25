2250bb2cf6fe0bc3dd6da0b4eca403ac8b50417b
Cardwin <ekschmidt11@gmail.com>
2020-01-24 18:29:37 -0700

    Add files via upload

---
+++ ExpressionManager.cpp
@@ -0,0 +1,9 @@
+# include "ExpressionManager.h"
+
+int ExpressionManager::postfixEvaluation() {
+
+}
+
+string ExpressionManager::postfixToInfix() {
+
+}
---
+++ ExpressionManager.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "ExpressionManagerInterface.h"
+
+using namespace std;
+
+class ExpressionManager {
+public:
+	bool IsBalanced();
+	int postfixEvaluate(); // Emily's
+	bool infixToPostfix();
+	string postfixToInfix(); // Emily's
+	string toString();
+
+private:
+
+};
