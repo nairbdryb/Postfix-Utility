42e1cc1e10846733f7c7ac87f3201b5883342e5f
Cardwin <ekschmidt11@gmail.com>
2020-01-24 19:10:45 -0700

    Update ExpressionManager.cpp

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -1,9 +1,20 @@
-# include "ExpressionManager.h"
+#include "ExpressionManager.h"
 
 int ExpressionManager::postfixEvaluation() {
-
+	// add numbers to stack until I get an operator. 
+	// then pop off the top 2 elements then put the operator in the middle, 
+	//top is right bottom left
 }
 
 string ExpressionManager::postfixToInfix() {
+	stack<char> postfix;
+	//add numbers to the top of the stack until you get to an operator
+	// check to see if it's less than 2 if so INVALID
+	// top number is right number 
+	// bottom number is left number
+	// operator in the middle
+	// add parentheses
+	// add to top of the stack
+	//continue
 
 }
