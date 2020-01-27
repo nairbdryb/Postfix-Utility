759d51291149531d8d804a08dfbea52abe203c91
Cardwin <ekschmidt11@gmail.com>
2020-01-27 01:35:00 -0700

    Fixed some Emily Stuff

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -10,18 +10,24 @@ int ExpressionManager::postfixEvaluation(string postfixExpression) { // Not your
 }
 
 string ExpressionManager::postfixToInfix(string postfixExpression) { //also not yours
-	stack<char> postfix;
+	stack<char> postfix; // can a stack of chars also hold strings?
 	for (int i = 0; postfixExpression.length(); ) { // continues until the entire string is done
 		if (postfixExpression.at(i) == " ") { // if the char is a space it is ignored
 			i++;
 		} //FIXME: how do I deal with double digits?
 		else if (isdigit(postfixExpression.at(i)) ) { // Sees is the char is a number
+			if (postfixExpression.at(i + 1) == " ") {
+				postfix.push(postfixExpression.at(i)); // If there is a space after the number then it takes it and adds it to the stack
+			}
+			else if (isdigit(postfixExpression.at(i + 1))) {
+				postfix.push(postfixExpression.at(i) && postfixExpression.at(i + 1));// I don't know if this is legal Maybe add to a different string variable first?
+			}
 			//TODO: if the next one is a space add to string		IF NOT then take both numbers and add them in one string
 
 		}
 		else if (postfixExpression.at(i) == "+" || postfixExpression.at(i) == "-" 
 			|| postfixExpression.at(i) == "*" || postfixExpression.at(i) == "/") { // if the char is an operand it is added to the top of the stack
-			
+			// TODO: find a way to make the things all add to a string. then add the string back into the stack.
 			postfix.push(postfixExpression.at(i))
 		}
 	}
