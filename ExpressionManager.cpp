ee97cfc7b5874a63f9a7c9c13843f1ca433104f2
Cardwin <ekschmidt11@gmail.com>
2020-01-26 19:30:13 -0700

    Update ExpressionManager.cpp

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -3,15 +3,28 @@
 
 using namespace std;
 
-int ExpressionManager::postfixEvaluation(string postfixExpression) {
+int ExpressionManager::postfixEvaluation(string postfixExpression) { // Not yours
 	// add numbers to stack until I get an operator. 
 	// then pop off the top 2 elements then put the operator in the middle, 
 	//top is right bottom left
 }
 
-string ExpressionManager::postfixToInfix(string postfixExpression) {
+string ExpressionManager::postfixToInfix(string postfixExpression) { //also not yours
 	stack<char> postfix;
-	while(postfixEspression)
+	for (int i = 0; postfixExpression.length(); ) { // continues until the entire string is done
+		if (postfixExpression.at(i) == " ") { // if the char is a space it is ignored
+			i++;
+		} //FIXME: how do I deal with double digits?
+		else if (isdigit(postfixExpression.at(i)) ) { // Sees is the char is a number
+			//TODO: if the next one is a space add to string		IF NOT then take both numbers and add them in one string
+
+		}
+		else if (postfixExpression.at(i) == "+" || postfixExpression.at(i) == "-" 
+			|| postfixExpression.at(i) == "*" || postfixExpression.at(i) == "/") { // if the char is an operand it is added to the top of the stack
+			
+			postfix.push(postfixExpression.at(i))
+		}
+	}
 
 
 	//add numbers to the top of the stack until you get to an operator
