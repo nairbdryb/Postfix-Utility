fa3933a6b77b0383eefb86cf36d203f471c7b8fe
babananabananana <babananabananana@outlook.com>
2020-01-28 17:26:59 -0700

    Add files via upload

--- ExpressionManager.cpp
+++ ExpressionManager.cpp
@@ -1,87 +1,48 @@
+//
+// Created by Ethan Low on 1/27/20.
+//
+
 #include "ExpressionManager.h"
-#include <string>
-#include <sstream>
+#include <stack>
+#include <iostream>
 
-using namespace std;
+bool ExpressionManager::isBalanced(string expression) {
+    stack<char> myStack;
+    //cin >> (myChar, expression);
+    for (int i = 0; i < expression.length(); i++) {
+        if (expression[i] == '(' || expression[i] == '{' || expression[i] == '[') {
+            myStack.push(expression[i]);
+        }
+        else if (expression[i] == ')' || expression[i] == '}' || expression[i] == ']'){
+            if (myStack.empty()){
+                return false;
+            }
+            if (expression[i] == ')' && myStack.top() == '('){
+                myStack.pop();
+            } else if (expression[i] == '}' && myStack.top() == '{') {
+                myStack.pop();
+            } else if (expression[i] == ']' && myStack.top() == '['){
+                myStack.pop();
+            } else return false;
+        } //else return false;
 
-ExpressionManager::ExpressionManager() {
-	//yayyyyyyyy we have no variables!
+    }
+    if (myStack.empty()){
+        return true;
+    } else return false;
 }
 
-string ExpressionManager::postfixEvaluate(string postfixExpression) { // Not yours
-	// add numbers to stack until I get an operator. 
-	// then pop off the top 2 elements then put the operator in the middle, 
-	//top is right bottom left
+string ExpressionManager::postfixToInfix(string postfixExpression) {
+    return std::string();
 }
-/*
-string ExpressionManager::postfixToInfix(string postfixExpression) { //also not yours
-	stack<char> postfix; // can a stack of chars also hold strings?
-	for (int i = 0; postfixExpression.length(); ) { // continues until the entire string is done
-		if (postfixExpression.at(i) == " ") { // if the char is a space it is ignored
-			i++;
-		} //FIXME: how do I deal with double digits?
-		else if (isdigit(postfixExpression.at(i)) ) { // Sees is the char is a number
-			if (postfixExpression.at(i + 1) == " ") {
-				postfix.push(postfixExpression.at(i)); // If there is a space after the number then it takes it and adds it to the stack
-			}
-			else if (isdigit(postfixExpression.at(i + 1))) {
-				postfix.push(postfixExpression.at(i) && postfixExpression.at(i + 1));// I don't know if this is legal Maybe add to a different string variable first?
-			}
-			//TODO: if the next one is a space add to string		IF NOT then take both numbers and add them in one string
 
-		}
-		else if (postfixExpression.at(i) == "+" || postfixExpression.at(i) == "-" 
-			|| postfixExpression.at(i) == "*" || postfixExpression.at(i) == "/") { // if the char is an operand it is added to the top of the stack
-			// TODO: find a way to make the things all add to a string. then add the string back into the stack.
-			postfix.push(postfixExpression.at(i))
-		}
-	}
-	
-	//add numbers to the top of the stack until you get to an operator
-	// check to see if it's less than 2 if so INVALID
-	// top number is right number 
-	// bottom number is left number
-	// operator in the middle
-	// add parentheses
-	// add to top of the stack
-	//continue
-
+string ExpressionManager::postfixEvaluate(string postfixExpression) {
+    return std::string();
 }
-*/
 
-string ExpressionManager::infixToPostFix(string infixExpression) {
-	/*
-	//stringstream ss;
-	stack<char> infix;
+string ExpressionManager::infixToPostfix(string infixExpression) {
 
-	//ss << infixExpression;
-	istringstream ss(infixExpression);
-	do {
-		//infix.push(ss);
-		//cout << infix.top();
-	} while (ss);
-	/*
-	pop()//tells you element and removes it
-	top()//tells you top element
-	push()//adds element to the top
-	empty()//checks to see if it is empty
-	*/
-	
-}
-int ExpressionManager::GetHierarchy(string input) {
-	if (input == ")" || input == "]" || input == "}") {
-		return 3;
-	}
-	else if (input == "*" || input == "/" || input == "%") {
-		return 2;
-	}
-	else if (input == "+" || input == "-") {
-		return 1;
-	}
-	else if (input == "(" || input == "[" || input == "{") {
-		return 0;
-	}
-	else {
-		return -1;
-	}
+
+
+    return std::string();
 }
--- ExpressionManager.h
+++ ExpressionManager.h
@@ -1,28 +1,61 @@
-#ifndef EXPRESSION_MANAGER
-
+#pragma once
 #include <iostream>
 #include <string>
+#include <stack>
 #include "ExpressionManagerInterface.h"
 
 using namespace std;
 
-class ExpressionManager : public ExpressionManagerInterface {
+
+class ExpressionManager : ExpressionManagerInterface
+{
 public:
-	ExpressionManager();
-	~ExpressionManager();
-	bool isBalanced(string expression);
-	string postfixEvaluate(string postfixExpression); // Not yours
-	//bool infixToPostfix(string infixExpression);
-	string postfixToInfix(string postfixExpression); //also not yours
-	string toString();
-	string infixToPostFix(string infixExpression); //mine
-	int GetHierarchy(string input); //also mine
+    ExpressionManager() {}
+    ~ExpressionManager() {}
+
+    bool isBalanced(string expression);
+
+    /**
+    * Converts a postfix expression into an infix expression
+    * and returns the infix expression.
+    *
+    * - The given postfix expression will have a space between every number or operator.
+    * - The returned infix expression must have a space between every number or operator.
+    * - Redundant parentheses are acceptable i.e. ( ( 3 * 4 ) + 5 ).
+    * - Check lab requirements for what will be considered invalid.
+    *
+    * return the string "invalid" if postfixExpression is not a valid postfix expression.
+    * otherwise, return the correct infix expression as a string.
+    */
+    string postfixToInfix(string postfixExpression);
+
+    /**
+    * Evaluates a postfix expression returns the result as a string
+    *
+    * - The given postfix expression will have a space between every number or operator.
+    * - Check lab requirements for what will be considered invalid.
+    *
+    * return the string "invalid" if postfixExpression is not a valid postfix Expression
+    * otherwise, return the correct evaluation as a string
+    */
+    string postfixEvaluate(string postfixExpression);
+
+    /**
+    * Converts an infix expression into a postfix expression
+    * and returns the postfix expression
+    *
+    * - The given infix expression will have a space between every number or operator.
+    * - The returned postfix expression must have a space between every number or operator.
+    * - Check lab requirements for what will be considered invalid.
+    *
+    * return the string "invalid" if infixExpression is not a valid infix expression.
+    * otherwise, return the correct postfix expression as a string.
+    */
+    string infixToPostfix(string infixExpression);
+
+
 
 private:
+  //  stack<char> myStack();
 
 };
-
-#define EXPRESSION_MANAGER
-#endif // !EXPRESSION_MANAGER
-
-
